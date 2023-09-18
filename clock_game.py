9a31aa0fb20af072982871458ef28309bce32650
Max Schain <mls2320@columbia.edu>
2023-09-18 00:14:01 -0400

    improved player but still bad

--- clock_game.py
+++ clock_game.py
@@ -7,7 +7,7 @@ import constants
 from players.default_player import Player as Default_Player
 #have to change inheritance for all of these
 from players.p1 import Player as p1
-from players.default_player import Player as p2
+from players.p2 import Player as p2
 from players.default_player import Player as p3
 from players.default_player import Player as p4
 from players.default_player import Player as p5
