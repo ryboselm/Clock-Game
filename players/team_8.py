63c47137331c8f19822fdca8c9cc3c08078fba71
giancarlo-pereira <85139465+giancarlo-pereira@users.noreply.github.com>
2023-09-20 13:00:55 -0400

    lowered # of rollouts to 1000

--- players/team_8.py
+++ players/team_8.py
@@ -207,7 +207,7 @@ class Player:
 
         return tree
 
-    def __MCTS(self, cards: list[str], constraints: list[str], state: list[str], rollouts: int = 5000):
+    def __MCTS(self, cards: list[str], constraints: list[str], state: list[str], rollouts: int = 1000):
         # MCTS main loop: Execute MCTS steps rollouts number of times
         # Then return successor with highest number of rollouts
         tree = Tree(Node(np.array(state), 24, 'Z', 0, 1))
